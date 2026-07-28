d547efeae41c16c7ee5e591beb91af1f212e61fc
raifanizzullhaq <raifanizzulhaq3>
2026-07-28 14:43:17 +0700

    Initialize global variables for linear search implementation

---
+++ linearsearch.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; //inisialisasi variabel arr sebagai variabel global 
+int n; //inisialisasi variabel n sebagai variabel global 
+int i; //inisialisasi variabel i sebagai variabel global
