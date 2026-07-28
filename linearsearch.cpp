71f479b77645dc7efff8288f4cd7f83db3733b3f
raifanizzullhaq <raifanizzulhaq3>
2026-07-28 15:06:52 +0700

    Refactor input procedure to validate array size and enhance user prompts

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]; //inisialisasi variabel arr sebagai variabel global 
-int n; //inisialisasi variabel n sebagai variabel global 
-int i; //inisialisasi variabel i sebagai variabel global
+int arr[20];   //inisialisasi variabel arr sebagai variabel global
+int n;         //inisialisasi variabel n
+int i;         //inisialisasi variabel i
+
+void input()   //membuat prosedur input
+{
+    while (true) //membuat looping untuk menentukan panjang element array
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 20): ";
+        cin >> n;
+        if (n > 0 && n <= 20) //membuat kondisi jika n > 0 dan n <= 20
+        {
+            break;
+        }
+        else //membuat kondisi jika n > 0 dan n <= 20 tidak terpenuhi
+        {
+            cout << "\nMinimum Jumlah element adalah 1 dan Maksimum Jumlah element adalah 20\n" << endl;
+        }
+    }
